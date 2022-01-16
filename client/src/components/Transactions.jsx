d02bbc3799e52ddd92b184643376bb4f11d4b5c2
SBagaria2710 <shashwawtforwork@gmail.com>
2022-01-16 13:32:05 +0530

    Services and transaction section

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,20 @@
+import React, { useContext } from 'react';
+
+import { TransactionContext } from '../context/TransactionContext';
+import dummyData from '../utils/dummyData';
+
 const Transactions = () => {
+
+    const { connectedAccount } = useContext(TransactionContext);
+
     return (
-        <h1>Transactions</h1>
+        <div className="flex items-center justify-center w-full 2xl:px-20 gradient-bg-transactions">
+            <div className="flex flex-col px-4 py-12 md:p-12">
+                <h3 className="my-2 text-3xl text-center text-white">
+                    {connectedAccount ? "Latest Tranactions" : "Connect your account to see the changes"}
+                </h3>
+            </div>
+        </div>
     );
 };
 
